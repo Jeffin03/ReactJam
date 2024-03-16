2caada74549a5f4494d267448d4bafc68663371b
Jeffin03 <jeffyissac2203@gmail.com>
2024-03-16 22:52:54 +0530

    added some styling

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -4,27 +4,31 @@ import "./App.css";
 export default function App() {
   const [weatherData, setWeatherData] = useState();
   const [location, setLocation] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const updateLocation = (e) => {
     e.preventDefault();
     const locationInput = document
       .getElementById("locationInput")
       .value.toLowerCase();
     setLocation(locationInput);
+    clear();
   };
 
   useEffect(() => {
     const fetchWeatherData = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
-          `https://api.weatherapi.com/v1/current.json?key=${
+          `https://api.weatherapi.com/v1/forecast.json?key=${
             import.meta.env.VITE_WEATHER_API_KEY
-          }&q=${location}`
+          }&q=${location}&days=7`
         );
         const data = await response.json();
         setWeatherData(data);
       } catch (error) {
         console.error("Error fetching weather data: ", error);
       }
+      setIsLoading(false);
     };
 
     if (location) {
@@ -32,20 +36,52 @@ export default function App() {
     }
   }, [location]);
 
+  function clear() {
+    document.getElementById("locationInput").value = "";
+  }
+
   return (
     <div>
-      <h1>Weather App</h1>
-      <form onSubmit={updateLocation}>
-        <label htmlFor="locationInput">Enter the Location Name: </label>
-        <input type="text" id="locationInput" />
-        <input type="submit" value="Search" />
-      </form>
-      {weatherData && (
-        <div>
-          <img src={weatherData.current.condition.icon} alt="weather icon" />
-          <p>{weatherData.current.temp_c}°C</p>
+      <div className="top-section">
+        <h1>Weather App</h1>
+        <form onSubmit={updateLocation}>
+          <input
+            type="text"
+            id="locationInput"
+            placeholder="Enter the location"
+          />
+          <input type="submit" value="Search" id="searchbtn" />
+        </form>
+      </div>
+      {isLoading ? (
+        <p id="loadingText">Loading...</p>
+      ) : weatherData ? (
+        <div className="weatherInfo">
+          <div className="weatherSpecs">
+            <div>
+              <h2 className="capitalize">{location}</h2>
+              <img
+                src={weatherData.current.condition.icon}
+                alt="weather icon"
+              />
+            </div>
+            <div>
+              <p>{weatherData.current.temp_c}°C</p>
+              <p>{weatherData.current.temp_f}°F</p>
+            </div>
+          </div>
+          <h2>Forecast</h2>
+          <div className="forecastInfo grid">
+            {weatherData.forecast.forecastday.map((day) => (
+              <div key={day.date} id="forecast">
+                <h3>{day.date}</h3>
+                <img src={day.day.condition.icon} alt="weather icon" />
+                <p>Temperature: {day.day.avgtemp_c}°C</p>
+              </div>
+            ))}
+          </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
